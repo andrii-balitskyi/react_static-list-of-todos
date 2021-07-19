bd9e8ae5f71bcb3606ec24067e26d7f8ccbe4706
Andrii Balitskyi <10balian10@gmail.com>
2021-07-19 20:44:53 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.56475be9cba24000b69e470ba1f5d46c.js"
+  "./precache-manifest.8fa491643ce4ef7b7b2121a331417c8f.js"
 );
 
 self.addEventListener('message', (event) => {
